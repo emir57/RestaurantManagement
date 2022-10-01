acc73ab9075375f4377207cb2b7bb9905fcc52af
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-01 15:18:28 +0300

    ProductDetail Persistence Created

---
+++ src/Services/ProductDetail/Services.ProductDetail.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Services.ProductDetail.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/Services/ProductDetail/Services.ProductDetail.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Services.ProductDetail.Persistence.Settings;
+
+namespace Services.ProductDetail.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            //services.Configure<DatabaseSettings>(configuration.GetSection("DatabaseSettings"));
+
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
+            return services;
+        }
+    }
+}
