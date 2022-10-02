6a3136f442365784183c9dab2eff99cccc58b6d1
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-02 16:10:21 +0300

    ProductsDetailsController Updated

--- src/Services/ProductDetail/Services.ProductDetail.API/Controllers/ProductsDetailsController.cs
+++ src/Services/ProductDetail/Services.ProductDetail.API/Controllers/ProductsDetailsController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Services.ProductDetail.Application.Features.ProductDetail.Commands.CreateProductsDetail;
+using Services.ProductDetail.Application.Features.ProductDetail.Commands.DeleteProductsDetail;
+using Services.ProductDetail.Application.Features.ProductDetail.Commands.UpdateProductsDetail;
+using Services.ProductDetail.Application.Features.ProductDetail.Dtos;
+using Services.ProductDetail.Application.Features.ProductDetail.Queries.GetAllProductsDetail;
+using Services.ProductDetail.Application.Features.ProductDetail.Queries.GetByIdProductsDetail;
 
 namespace Services.ProductDetail.API.Controllers
 {
@@ -7,10 +12,39 @@ namespace Services.ProductDetail.API.Controllers
     [ApiController]
     public class ProductsDetailsController : BaseController
     {
-        [HttpPost]
-        public async Task<IActionResult> Post(CreateProductsDetailCommand createProductsDetailCommand)
+        [HttpGet("get/byid")]
+        public async Task<IActionResult> GetById([FromQuery] GetByIdProductsDetailQuery getByIdProductsDetailQuery)
+        {
+            ReadProductsDetailDto readProductsDetailDto = await Mediator.Send(getByIdProductsDetailQuery);
+            return Ok(readProductsDetailDto);
+        }
+
+        [HttpGet("get/all")]
+        public async Task<IActionResult> GetAll()
+        {
+            ListReadProductsDetailDto listReadProductsDetailDto = await Mediator.Send(new GetAllProductsDetailQuery());
+            return Ok(listReadProductsDetailDto);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(CreateProductsDetailCommand createProductsDetailCommand)
         {
             await Mediator.Send(createProductsDetailCommand);
+            return NoContent();
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromQuery] string id, [FromBody] WriteProductsDetailDto writeProductsDetailDto)
+        {
+            UpdateProductsDetailCommand updateProductsDetailCommand = new() { Id = id, WriteProductsDetailDto = writeProductsDetailDto };
+            await Mediator.Send(updateProductsDetailCommand);
+            return NoContent();
+        }
+
+        [HttpPost("delete")]
+        public async Task<IActionResult> Delete([FromQuery] DeleteProductsDetailCommand deleteProductsDetailCommand)
+        {
+            await Mediator.Send(deleteProductsDetailCommand);
             return NoContent();
         }
 
