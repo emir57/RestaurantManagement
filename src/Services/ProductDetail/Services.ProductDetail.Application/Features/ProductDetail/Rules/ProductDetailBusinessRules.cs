a50be7b9da7f0cc016a1ca933ebd19500ffb082b
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-02 15:36:39 +0300

    ProductsDetail BusinessRules and Messages Created

---
+++ src/Services/ProductDetail/Services.ProductDetail.Application/Features/ProductDetail/Rules/ProductDetailBusinessRules.cs
@@ -0,0 +1,42 @@
+﻿using Services.ProductDetail.Application.Features.ProductDetail.Constants;
+using Services.ProductDetail.Application.Features.ProductDetail.Dtos;
+using Services.ProductDetail.Application.Services;
+using Services.ProductDetail.Domain.Entities;
+
+namespace Services.ProductDetail.Application.Features.ProductDetail.Rules
+{
+    public class ProductDetailBusinessRules
+    {
+        private readonly IProductDetailService _productDetailService;
+        private readonly ProductDetailBusinessRulesMessages _messages;
+
+        public ProductDetailBusinessRules(ProductDetailBusinessRulesMessages messages, 
+                                          IProductDetailService productDetailService)
+        {
+            _messages = messages;
+            _productDetailService = productDetailService;
+        }
+
+        public async Task ProductsDetailNameShouldNotBeAlreadyExistWhenProductsDetailCreated(string name)
+        {
+
+        }
+
+        public async Task ProductsDetailNameShouldNotBeAlreadyExistWhenProductsDetailUpdated(string name)
+        {
+
+        }
+
+        public async Task AllProductExtrasShouldBeAlreadyExistWhenProductsDetailCreated(ICollection<WriteProductExtrasDto> productExtras)
+        {
+
+        }
+
+        public async Task ProductsDetailShouldBeExistsWhenRequestedWithId(string id)
+        {
+            ProductsDetail productsDetail = await _productDetailService.GetAsync(p => p.Id == id);
+
+            if (productsDetail == null) ; //Need to throw after core exception created.
+        }
+    }
+}
