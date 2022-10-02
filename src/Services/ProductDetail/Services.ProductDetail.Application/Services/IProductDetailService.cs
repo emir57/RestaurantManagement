e82c99f1e090e4981698d955947207835f6020cd
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-02 14:46:58 +0300

    Services Updated

--- src/Services/ProductDetail/Services.ProductDetail.Application/Services/IProductDetailService.cs
+++ src/Services/ProductDetail/Services.ProductDetail.Application/Services/IProductDetailService.cs
@@ -1,10 +1,12 @@
 ﻿using Services.ProductDetail.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace Services.ProductDetail.Application.Services
 {
     public interface IProductDetailService
     {
-        Task<ProductsDetail> GetByIdAsync(string id);
+        Task<List<ProductsDetail>> GetListAsync(Expression<Func<ProductsDetail, bool>> predicate);
+        Task<ProductsDetail> GetAsync(Expression<Func<ProductsDetail, bool>> predicate);
         Task AddAsync(ProductsDetail productDetail);
         Task UpdateAsync(ProductsDetail productDetail);
         Task DeleteAsync(string id);
--- src/Services/ProductDetail/Services.ProductDetail.Application/Services/ProductDetailService.cs
+++ src/Services/ProductDetail/Services.ProductDetail.Application/Services/ProductDetailService.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using Services.ProductDetail.Domain.Entities;
 using Services.ProductDetail.Persistence.Settings;
+using System.Linq.Expressions;
 
 namespace Services.ProductDetail.Application.Services
 {
@@ -16,19 +17,33 @@ namespace Services.ProductDetail.Application.Services
             _productsDetailCollections = database.GetCollection<ProductsDetail>(databaseSettings.ProductDetailCollectionName);
         }
 
+        public async Task<List<ProductsDetail>> GetListAsync(Expression<Func<ProductsDetail, bool>> predicate)
+        {
+            List<ProductsDetail> productsDetailList = await _productsDetailCollections.Find(predicate).ToListAsync();
+            //need to fill "ProductExtras" and "Feature" after other microservices done.
+            return productsDetailList;
+        }
+
+        public async Task<ProductsDetail> GetAsync(Expression<Func<ProductsDetail, bool>> predicate)
+        {
+            ProductsDetail productsDetail = await _productsDetailCollections.Find(predicate).FirstAsync();
+            //need to fill "ProductExtras" and "Feature" after other microservices done.
+            return productsDetail;
+        }
+
         public async Task AddAsync(ProductsDetail productDetail)
         {
             await _productsDetailCollections.InsertOneAsync(productDetail);
         }
 
-        public Task DeleteAsync(string id)
+        public async Task DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            await _productsDetailCollections.DeleteOneAsync(x => x.Id == id);
         }
 
-        public Task UpdateAsync(ProductsDetail productDetail)
+        public async Task UpdateAsync(ProductsDetail productDetail)
         {
-            throw new NotImplementedException();
+            await _productsDetailCollections.FindOneAndReplaceAsync(x => x.Id == productDetail.Id, productDetail);
         }
     }
 }
