3d817e7723269774b7b3aa356b0e86de08439d25
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-02 14:20:31 +0300

    Dtos Created

---
+++ src/Services/ProductDetail/Services.ProductDetail.Application/Features/ProductDetail/Dtos/ReadProductsDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.ProductDetail.Application.Features.ProductDetail.Dtos;
+
+public sealed class ReadProductsDetailDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string ShortDescription { get; set; }
+    public decimal Price { get; set; }
+    public string Image { get; set; }
+    public ReadFeatureDto Feature { get; set; }
+    public string LongDescription { get; set; }
+    public ICollection<ReadProductExtrasDto> ProductExtras { get; set; }
+}
--- src/Services/ProductDetail/Services.ProductDetail.Application/Services/IProductDetailService.cs
+++ src/Services/ProductDetail/Services.ProductDetail.Application/Services/IProductDetailService.cs
@@ -4,6 +4,7 @@ namespace Services.ProductDetail.Application.Services
 {
     public interface IProductDetailService
     {
+        Task<ProductsDetail> GetByIdAsync(string id);
         Task AddAsync(ProductsDetail productDetail);
         Task UpdateAsync(ProductsDetail productDetail);
         Task DeleteAsync(string id);
